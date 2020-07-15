52a61fdbd8d7d8487088288faeb67355bff1fd4b
wangchen <wangchen@huadong.net>
2020-07-15 15:44:48 +0800

    代码初始化

---
+++ src/main/java/org/wang-2345/chen/App.java
@@ -0,0 +1,13 @@
+package org.wang-2345.chen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
