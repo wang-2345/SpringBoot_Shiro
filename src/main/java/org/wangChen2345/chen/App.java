b0b4f9f721699703c633aa707b7bd39c248bd873
wangchen <wangchen@huadong.net>
2020-07-16 10:21:12 +0800

    修正文件名

---
+++ src/main/java/org/wangChen2345/chen/App.java
@@ -0,0 +1,13 @@
+package org.wangChen2345.chen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
